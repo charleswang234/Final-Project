150edfb19627cf7f973fa560ed341f9df62c2b35
charleswang234 <charleswang234@gmail.com>
2017-06-01 12:39:40 -0400

    created greenfoot project

---
+++ Raft Battles/MyWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Main world for Raft Battles
+ * 
+ * @author Charles Wang
+ * @version June 2017
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1, false); 
+    }
+}
